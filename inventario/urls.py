43da0336aa2adaba71e682b526872d715d867a16
natcore-tech <elihu.navarrete@ute.edu.ec>
2026-06-29 16:38:36 -0500

    error static root

--- inventario/urls.py
+++ inventario/urls.py
@@ -60,7 +60,7 @@ urlpatterns = [
     path('auth/token/refresh/', TokenRefreshView.as_view()),
     path('auth/token/verify/', TokenVerifyView.as_view()),
     path('auth/logout/', LogoutView.as_view()),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('', include(router.urls)),
 ]
